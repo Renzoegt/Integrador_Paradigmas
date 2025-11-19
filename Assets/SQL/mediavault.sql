8d94292955b548bb6f96bde38cd9bd275176f78f
Yume <150016933+Renzoegt@users.noreply.github.com>
2025-11-19 14:46:18 -0300

    Update mediavault.sql

--- Assets/SQL/mediavault.sql
+++ Assets/SQL/mediavault.sql
@@ -8,12 +8,11 @@ CREATE TABLE usuarios (
     password VARCHAR(255) NOT NULL
 );
 
--- Tabla de contenido (reemplaza 'archivos')
+-- Tabla de contenido
 CREATE TABLE contenido (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     descripcion VARCHAR(500),
-
     archivo VARCHAR(500),   -- ruta al archivo subido
     url VARCHAR(500),       -- URL externa opcional
     miniatura VARCHAR(500) DEFAULT "Integrador_Paradigmas/Assets/Images/placeholder.jpg",  -- ruta a la miniatura
@@ -21,12 +20,4 @@ CREATE TABLE contenido (
     usuario_id INT NOT NULL,
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
-);
-
--- Tabla de categorías
-CREATE TABLE categorias (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,       -- textos / videos / imagenes
-    titulo VARCHAR(255) NOT NULL,       -- Jorge Luis Borges, Terror clásico...
-    miniatura VARCHAR(255) NULL         -- imagen para el index
 );
